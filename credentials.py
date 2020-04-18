efb2df3be5b36f61ea5f438a0b22d4c3e74b4150
Jake Brown <jakebrown188@gmail.com>
2020-04-18 14:43:13 -0700

    Get following from my account

---
+++ credentials.py
@@ -0,0 +1,6 @@
+class Credentials:
+  def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+    self.consumer_key = consumer_key
+    self.consumer_secret = consumer_secret
+    self.access_token = access_token
+    self.access_token_secret = access_token_secret
--- follow_all_following.py
+++ follow_all_following.py
@@ -1,15 +1,44 @@
-import os
+# from twitter import Twitter
+import json
 import twitter
 
-consumer_key = os.environ.get('CONSUMER_KEY')
-print(consumer_key)
+from credentials import Credentials
 
-# consumer_secret = os.environ["CONSUMER_SECRET"]
-# access_token = os.environ["ACCESS_TOKEN"]
-# access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
-#
-#
-# api = twitter.Api(consumer_key=[consumer_key],
-#                   consumer_secret=[consumer_secret],
-#                   access_token_key=[access_token],
-#                   access_token_secret=[access_token_secret])
+CRED_FILE_NAME = "dontpush.json"
+
+
+def gather_credentials(cred_file):
+  try:
+    cred_file = open(CRED_FILE_NAME, 'r')
+  except FileNotFoundError:
+    print("No credential json file")
+    exit(1)
+
+  json_creds = json.load(cred_file)
+
+  return Credentials(
+    json_creds["consumer_key"],
+    json_creds["consumer_secret"],
+    json_creds["access_token"],
+    json_creds["access_token_secret"]
+  )
+
+
+def setup_api(credentials):
+  return twitter.Api(consumer_key=credentials.consumer_key,
+                     consumer_secret=credentials.consumer_secret,
+                     access_token_key=credentials.access_token,
+                     access_token_secret=credentials.access_token_secret)
+
+
+def main():
+  credentials = gather_credentials(CRED_FILE_NAME)
+  api = setup_api(credentials)
+  followers = api.GetFriends()
+  for follower in followers:
+    print(follower.screen_name + ", " + follower.name)
+  print("Total:", len(followers))
+
+
+if __name__ == '__main__':
+  main()
